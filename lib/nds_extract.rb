60478872351ba5e7d761930eaf7abb453c5aa041
Zac Uslianer <zacuslianer@gmail.com>
2020-02-06 15:46:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -14,6 +14,8 @@ end
 # The return value should be like:
 #
 # { directorOne => allTheMoneyTheyMade, ... }
+
+
 def directors_totals(nds)
   pp nds
   director_index = 0
@@ -21,27 +23,13 @@ def directors_totals(nds)
   while director_index < nds.count do
     total = 0 
     director_name = nds[director_index][:name]
+    movie_index = 0 
+    while movie_index < nds[director_index][:movies].count do 
+      total += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
+    end 
     result[director_name] = total 
     director_index += 1 
-    total += gross_for_director(director_data)  
   end
-   result
+  result
 end
-
-# def directors_totals(nds)
-#   pp nds
-#   director_index = 0
-#   result = {}
-#   while director_index < nds.count do
-#     total = 0 
-#     director_name = nds[director_index][:name]
-#     movie_index = 0 
-#     while movie_index < nds[director_index][:movies].count do 
-#       total += nds[director_index][:movies][movie_index][:worldwide_gross]
-#       movie_index += 1 
-#     end 
-#     result[director_name] = total 
-#     director_index += 1 
-#   end
-#   result
-# end
