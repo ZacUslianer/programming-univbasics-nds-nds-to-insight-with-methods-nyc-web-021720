70dc377419253439cb7a42c73c55e32401090e07
Zac Uslianer <zacuslianer@gmail.com>
2020-02-06 15:18:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,3 +15,23 @@ def directors_totals(nds)
   result = {}
   nil
 end
+
+
+
+def directors_totals(nds)
+  pp nds
+  director_index = 0
+  result = {}
+  while director_index < nds.count do
+    total = 0 
+    director_name = nds[director_index][:name]
+    movie_index = 0 
+    while movie_index < nds[director_index][:movies].count do 
+      total += nds[director_index][:movies][movie_index][:worldwide_gross]
+      movie_index += 1 
+    end 
+    result[director_name] = total 
+    director_index += 1 
+  end
+   result
+end
